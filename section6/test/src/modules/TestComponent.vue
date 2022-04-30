86664442050cc586fc330f5a25c1bc07518c1de5
tokuyama shouta <toku1031ts@gmail.com>
2022-04-30 17:09:15 +0900

    SingleFileComponent

---
+++ section6/test/src/modules/TestComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>テストです{{testData}}</div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  data(){
+      return{
+          testData:'テストdataです'
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
