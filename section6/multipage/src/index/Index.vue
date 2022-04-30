30ff20460b68ab7fc4984b224dde070a4de2ea8f
tokuyama shouta <toku1031ts@gmail.com>
2022-04-30 18:32:07 +0900

    multipage

---
+++ section6/multipage/src/index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+      Index<br>
+      <a href="index.html">index</a><br>
+      <a href="users.html">users</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ section6/multipage/src/users/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Users from './Users.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(Users),
+}).$mount('#app')
