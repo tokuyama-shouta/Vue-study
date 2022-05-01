e2400d2a0b30cda4e1374974f3fb041916de15d8
tokuyama shouta <toku1031ts@gmail.com>
2022-05-01 20:54:34 +0900

    動的パラメーターとprops

---
+++ section7/vuerouter/src/components/BookDetail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <p>本の詳細</p>
+      <p>タイトル：{{ title}}</p>
+      <p>本の内容：{{content}}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props:{
+      id: Number,
+      title: String,
+      content: String
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ section7/vuerouter/src/views/BookList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+      <h1>本の一覧</h1>
+      <ul>
+          <li @click="showBookDetail(book.id)"
+          v-for="book in books" :key="book.id">
+              {{ book.title}}
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BookList',
+    data(){
+        return{
+            bookIndex: -1,
+            books:[
+                {id:1, title:'タイトル1', content:'本の内容1'},
+                {id:2, title:'タイトル2', content:'本の内容2'},
+                {id:3, title:'タイトル3', content:'本の内容3'}
+            ]
+        }
+    },
+    methods:{
+        showBookDetail(id){
+            this.bookIndex = id - 1
+            this.$router.push({
+                name:'Book',
+                params:{
+                    id: this.books[this.bookIndex].id,
+                    title: this.books[this.bookIndex].title,
+                    content: this.books[this.bookIndex].content
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
