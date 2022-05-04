2cd11eecfbbcc19dd636b7b5da282b6dca460325
tokuyama shouta <toku1031ts@gmail.com>
2022-05-04 21:14:43 +0900

    app.vue localstorage

--- section8/bookapp/src/App.vue
+++ section8/bookapp/src/App.vue
@@ -1,26 +1,64 @@
 <template>
   <v-app>
-   <Header/>
+    <Header />
     <v-main>
-      <router-view/>
+      <v-container>
+        <router-view @add-book-list="addBook" />
+      </v-container>
     </v-main>
-    <Footer/>
+    <Footer />
   </v-app>
 </template>
 
 <script>
-import Header from '@/global/Header.vue';
-import Footer from '@/global/Footer.vue'
+import Header from "@/global/Header.vue";
+import Footer from "@/global/Footer.vue";
+const STORAGE_KEY = "books";
 export default {
-  name: 'App',
-
-  components:{
+  name: "App",
+  components: {
     Header,
     Footer
   },
-
-  data: () => ({
-    //
-  }),
-};
+  data() {
+    return {
+       books: [],
+       newBook: null
+    }
+  },
+  mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      } catch (e) {
+        localStorage.removeItem(STORAGE_KEY);
+      }
+    }
+  },
+  methods: {
+    addBook(e) {
+      this.books.push({
+        id: this.books.length,
+        title: e.title,
+        image: e.image,
+        description: e.description,
+        readDate: '',
+        memo: ''
+      });
+      // this.newBook = "";
+      this.saveBooks();
+    },
+    removeBook(x) {
+      this.cats.splice(x, 1);
+      this.saveBooks();
+    },
+    saveBooks() {
+      const parsed = JSON.stringify(this.books);
+      localStorage.setItem(STORAGE_KEY, parsed);
+    },
+    goToEditPage(id){
+      this.$router.push(`/edit/${id}`)
+    }
+  }
+}
 </script>
--- section8/bookapp/src/pages/BookSearch.vue
+++ section8/bookapp/src/pages/BookSearch.vue
@@ -1,47 +1,45 @@
 <template>
   <div>
-    <v-container>
-      <v-row>
-        <v-col cols="6">
-          <v-text-field label="本のタイトルを検索" v-model="keyword">
-          </v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="3">
-          <v-btn color="primary" @click="search(keyword)">検索する</v-btn>
-        </v-col>
-        <v-col cols="3">
-          <v-btn color="secondary" to="/">一覧に戻る</v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col
-          cols="12"
-          md="6"
-          v-for="(book, index) in searchResults"
-          :key="book.index"
-        >
-          <v-card class="mx-auto">
-            <v-row>
-              <v-col cols="4">
-                <v-img :src="book.image"></v-img>
-              </v-col>
-              <v-col cols="8">
-                <v-card-title>{{ book.title }}</v-card-title>
-                {{ book.description }}
-                <v-spacer></v-spacer>
-                <v-card-actions>
-                  <v-btn fab dark color="indigo" @click="addBookList(index)">
-                    <v-icon dark> mdi-plus </v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-row>
+      <v-col cols="6">
+        <v-text-field label="本のタイトルを検索" v-model="keyword">
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3">
+        <v-btn color="primary" @click="search(keyword)">検索する</v-btn>
+      </v-col>
+      <v-col cols="3">
+        <v-btn color="secondary" to="/">一覧に戻る</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12"
+        md="6"
+        v-for="(book, index) in searchResults"
+        :key="book.index"
+      >
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title>{{ book.title }}</v-card-title>
+              {{ book.description }}
+              <v-spacer></v-spacer>
+              <v-card-actions>
+                <v-btn fab dark color="indigo" @click="addBookList(index)">
+                  <v-icon dark>mdi-plus</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -50,11 +48,15 @@ export default {
   name: "BookSearch",
   data() {
     return {
-      keyword: "",
+      keyword: '',
       searchResults: [],
     };
   },
   methods: {
+    addBookList(index){
+      this.$emit('add-book-list', this.searchResults[index])
+      console.log("ii")
+    },
     async search(keyword) {
       this.searchResults = [];
       // クエリーストリングを作成
@@ -64,13 +66,13 @@ export default {
         maxResults: 40,
       };
       const queryParams = new URLSearchParams(params);
-      console.log(baseUrl + queryParams);
+      // console.log(baseUrl + queryParams);
 
       // fetchでjson取得
       const response = await fetch(baseUrl + queryParams).then((response) =>
         response.json()
       );
-      console.log(response.items);
+      // console.log(response.items);
 
       // 必要な情報を配列にpush
       for (let book of response.items) {
@@ -78,9 +80,9 @@ export default {
         let img = book.volumeInfo.imageLinks;
         let description = book.volumeInfo.description;
         this.searchResults.push({
-          title: title ? title : "",
-          image: img ? img.thumbnail : "",
-          description: description ? description.slice(0, 40) : "",
+          title: title ? title : '',
+          image: img ? img.thumbnail : '',
+          description: description ? description.slice(0, 40) : '',
         });
       }
     },
