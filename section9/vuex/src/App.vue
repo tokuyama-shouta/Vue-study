dbad6898142184bb430ab26d5da6ac4f16673b71
tokuyama shouta <toku1031ts@gmail.com>
2022-05-05 20:16:55 +0900

    getters

--- section9/vuex/src/App.vue
+++ section9/vuex/src/App.vue
@@ -6,8 +6,26 @@
     </nav>
     <router-view/>
     {{ $store.state.count}}
+    <br>
+    <ul>
+      <li v-for="user in visibleUsers" :key="user.id">
+        {{ user.id }} : {{ user.name }} : {{ user.isVisible}}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+
+export default{
+  computed:{
+    visibleUsers(){
+      return this.$store.getters.visibleUsers
+    }
+  }
+}
+</script>
+
 
 <style>
 
--- section9/vuex/src/store/index.js
+++ section9/vuex/src/store/index.js
@@ -6,9 +6,17 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     //初期値
-    count: 0
+    count: 0,
+    users:[
+      {id:1, name:'大谷', isVisible: true},
+      {id:2, name:'ダルビッシュ', isVisible: true},
+      {id:3, name:'錦織', isVisible: true},
+    ]
   },
   getters: {
+    visibleUsers(state){
+      return state.users.filter(user => user.isVisible)
+    },
   },
   mutations: {
     increment(state){
